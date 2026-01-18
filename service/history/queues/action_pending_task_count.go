8ecb3363e7dbc299ba85fcd7c0c9deafdbc93e34
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-18 12:21:47 -0500

    spelling: corresponding
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- service/history/queues/action_pending_task_count.go
+++ service/history/queues/action_pending_task_count.go
@@ -136,7 +136,7 @@ func (a *actionQueuePendingTask) findSliceToClear(
 
 		sliceList := a.slicesPerKey[key]
 		if len(sliceList) == 0 {
-			panic("Found key with non-zero pending task count but has no correspoding Slice")
+			panic("Found key with non-zero pending task count but has no corresponding Slice")
 		}
 
 		// pop the first slice in the list
