f01f6d53b4f27a6cd64e7d6da4e0ed515f275472
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-18 20:38:39 -0500

    spelling: inherited
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/task_queue_stats_test.go
+++ tests/task_queue_stats_test.go
@@ -1371,7 +1371,7 @@ func (s *TaskQueueStatsSuite) enqueueActivitiesForEachWorkflow(sets int, tqName 
 									TaskQueue:             &taskqueuepb.TaskQueue{Name: tqName, Kind: enumspb.TASK_QUEUE_KIND_NORMAL},
 									StartToCloseTimeout:   durationpb.New(time.Minute),
 									RequestEagerExecution: false,
-									// Priority is inherted from the workflow
+									// Priority is inherited from the workflow
 								},
 							},
 						},
