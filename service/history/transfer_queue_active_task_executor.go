e1b32e21db256994901044b114c64ac78df6a267
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-20 22:11:09 -0500

    spelling: successfully
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- service/history/transfer_queue_active_task_executor.go
+++ service/history/transfer_queue_active_task_executor.go
@@ -971,7 +971,7 @@ func (t *transferQueueActiveTaskExecutor) processStartChildExecution(
 		}
 	}
 
-	// Note: childStarted flag above is computed from the parent's history. When this is TRUE it's guaranteed that the child was succesfully started.
+	// Note: childStarted flag above is computed from the parent's history. When this is TRUE it's guaranteed that the child was successfully started.
 	// But if it's FALSE then the child *may or may not* be started (ex: we failed to record ChildExecutionStarted event previously.)
 	// Hence we need to check the child workflow ID and attempt to reconnect before proceeding to start a new instance of the child.
 	// This path is usually taken when the parent is being reset and the reset point (i.e baseWorkflowInfo.LowestCommonAncestorEventId) is after the child was initiated.
