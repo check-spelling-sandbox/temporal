470773fcc6dfcab02b952a01d5266f48906618bc
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-20 22:06:45 -0500

    spelling: retrieves
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- common/archiver/gcloud/connector/client.go
+++ common/archiver/gcloud/connector/client.go
@@ -113,7 +113,7 @@ func (s *storageWrapper) Get(ctx context.Context, URI archiver.URI, fileName str
 	return io.ReadAll(reader)
 }
 
-// Query, retieves file names by provided storage query
+// Query, retrieves file names by provided storage query
 func (s *storageWrapper) Query(ctx context.Context, URI archiver.URI, fileNamePrefix string) (fileNames []string, err error) {
 	fileNames = make([]string, 0)
 	bucket := s.client.Bucket(URI.Hostname())
@@ -132,7 +132,7 @@ func (s *storageWrapper) Query(ctx context.Context, URI archiver.URI, fileNamePr
 
 }
 
-// QueryWithFilters, retieves filenames that match filter parameters. PageSize is optional, 0 means all records.
+// QueryWithFilters, retrieves filenames that match filter parameters. PageSize is optional, 0 means all records.
 func (s *storageWrapper) QueryWithFilters(ctx context.Context, URI archiver.URI, fileNamePrefix string, pageSize, offset int, filters []Precondition) ([]string, bool, int, error) {
 	var err error
 	currentPos := offset
