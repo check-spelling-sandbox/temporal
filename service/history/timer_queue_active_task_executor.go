7502593c851420243025f39a4d9f8aa96dd86b48
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-20 22:18:28 -0500

    spelling: expire
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- service/history/timer_queue_active_task_executor.go
+++ service/history/timer_queue_active_task_executor.go
@@ -243,7 +243,7 @@ Loop:
 	for _, timerSequenceID := range timerSequence.LoadAndSortActivityTimers() {
 		if !queues.IsTimeExpired(task, referenceTime, timerSequenceID.Timestamp) {
 			// timer sequence IDs are sorted, once there is one timer
-			// sequence ID not expired, all after that wil not expired
+			// sequence ID not expired, all after that wil not expire
 			break Loop
 		}
 
