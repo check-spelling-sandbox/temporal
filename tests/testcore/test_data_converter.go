4d9c3949cfa0be53617f13cfb3233319d64e530c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-25 00:00:11 -0500

    golangci staticcheck: ST1020 (move todo)
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/testcore/test_data_converter.go
+++ tests/testcore/test_data_converter.go
@@ -21,8 +21,8 @@ type TestDataConverter struct {
 	NumOfCallFromPayloads int
 }
 
-// TODO (alex): use it by default SdkClient everywhere?
 func NewTestDataConverter() converter.DataConverter {
+	// TODO (alex): use it by default SdkClient everywhere?
 	return &TestDataConverter{}
 }
 
